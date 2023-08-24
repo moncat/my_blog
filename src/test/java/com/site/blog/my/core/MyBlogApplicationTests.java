a9ff3cefdb832f579320ff55b0d3bbe31d01b184
moncat <moncat@users.noreply.github.com>
2023-08-24 14:20:21 +0800

    删除完整测试

--- src/test/java/com/site/blog/my/core/MyBlogApplicationTests.java
+++ src/test/java/com/site/blog/my/core/MyBlogApplicationTests.java
@@ -1,20 +1,12 @@
 package com.site.blog.my.core;
 
 import org.junit.jupiter.api.Test;
-import org.moon.tools.DemoUtils;
-import org.moon.tools.MoMapUtil;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.HashMap;
 
 @SpringBootTest
 public class MyBlogApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		System.out.println("test");
 	}
-
-
-
 }
