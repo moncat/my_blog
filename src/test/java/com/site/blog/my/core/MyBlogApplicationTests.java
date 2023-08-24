12d2deab4aa5c8fc2b63e8e5d68773aed23690ec
moncat <moncat@users.noreply.github.com>
2023-08-24 13:44:26 +0800

    添加谷歌广告

--- src/test/java/com/site/blog/my/core/MyBlogApplicationTests.java
+++ src/test/java/com/site/blog/my/core/MyBlogApplicationTests.java
@@ -1,13 +1,28 @@
 package com.site.blog.my.core;
 
 import org.junit.jupiter.api.Test;
+import org.moon.tools.DemoUtils;
+import org.moon.tools.MoMapUtil;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
 
 @SpringBootTest
 public class MyBlogApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		System.out.println("test");
 	}
 
+
+		public static void main(String[] args) {
+			String ss = DemoUtils.print("123");
+			System.out.println(ss);
+			String map = MoMapUtil.map("123455");
+			System.out.println(map);
+			final HashMap ddd = MoMapUtil.map2("ddd");
+			ddd.forEach((key, value) -> System.out.println(key + " -> " + value));
+
+		}
 }
